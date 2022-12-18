119c32624a37ec40ed5adc3bf97e26a5bf06dc03
Maha_hamoud <maahaa.fff@gmail.com>
2022-12-18 17:14:19 +0300

    Maha commit

---
+++ src/main/java/B9_Group2.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author mahahamoud
+ */
+public class B9_Group2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello i'm Maha (: ");
+    }
+}
