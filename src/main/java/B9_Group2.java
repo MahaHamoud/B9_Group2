488d1f27039ed4c293759017eb5ac3381ec71ec8
hadeel <hobakhshwen@stu.kau.edu.sa>
2022-12-18 17:39:04 +0300

    hadeel commit

--- src/main/java/B9_Group2.java
+++ src/main/java/B9_Group2.java
@@ -11,5 +11,6 @@ public class B9_Group2 {
     public static void main(String[] args) {
         System.out.println("Hello i'm Maha (: ");
         System.out.println("mzoun done");
+        System.out.println("hadeel done");
     }
 }
